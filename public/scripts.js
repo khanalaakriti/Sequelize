f1c162a106e427e53034527b8a8dedd4d2234bfe
khanalaakriti <66918681+khanalaakriti@users.noreply.github.com>
2021-03-26 23:42:49 -0400

    Updated

--- public/scripts.js
+++ public/scripts.js
@@ -1,4 +1,4 @@
-async function aakriti() {
+async function populateRestaurants() {
     const request = await fetch('/api/dining');
     const data = await request.json();
     const allDinersArray = data.data;
@@ -8,7 +8,7 @@ async function aakriti() {
 
     allDinersArray.forEach(diner => {
         const appendItem= document.createElement('li');
-        appendItem.classList.add('redColor');
+        appendItem.classList.add('black');
         appendItem.classList.add('boldText');
         const dinerHallId = diner.hall_id
         const dinerHallName = diner.hall_name
@@ -17,11 +17,23 @@ async function aakriti() {
         targetDiners.append(appendItem);
     })
 
+        // const appendItem = document.createElement('li');
+        // appendItem.classList.add('block');
+        // appendItem.classList.add('list-item');
+        // appendItem.classList.add('box');
+        // appendItem.classList.add('dark-background');
+        // appendItem.classList.add('light-text');
+        // appendItem.classList.add('mt-10');
+        // appendItem.innerHTML= `<div class="list-header is-size-5">${item.name}</div><div class="is-size-6 address">${item.address_line_1}</div>`;
+        // suggestions.append(appendItem);
+
+
+    
 };
   
 async function windowActions() {
     console.log('window loaded <-------------');
-    await aakriti();
+    await populateRestaurants();
 }
 
   
